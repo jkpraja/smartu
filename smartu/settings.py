d876b89fb6419fbe3b100b259f7f1b7779408aed
kalingga <jati.k.praja@gmail.com>
2020-02-18 12:08:48 +0700

    update Procfile

--- smartu/settings.py
+++ smartu/settings.py
@@ -81,10 +81,14 @@ WSGI_APPLICATION = 'smartu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.postgresql.psycopg2',
     }
 }
+
+DATABASES['default'] = dj_database_url.config(default='')
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
